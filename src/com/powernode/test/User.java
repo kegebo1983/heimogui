bfddcc7d4c683924e0e0bef5db91696b80c4313f
“kegebo1983” <1609630822@qq.com>
2021-01-22 15:12:08 +0800

    初始化提交

---
+++ src/com/powernode/test/User.java
@@ -0,0 +1,15 @@
+package com.powernode.test;
+
+/**
+ * @ProjectName: jdbc
+ * @Package: com.powernode.test
+ * @Description: java类作用描述
+ * @Author: 王勇刚
+ * @CreateDate: 2021/1/22 14:56
+ * @Version: 1.0
+ * <p>
+ * Copyright: Copyright (c) 2021
+ */
+public class User {
+    private int id;
+}
